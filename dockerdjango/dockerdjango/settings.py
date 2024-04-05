20c37dfa630c897a5dae25c72de53a802cf5d060
Michel SADEU <michelsadeungakou@gmail.com>
2024-04-05 11:00:47 +0200

    Add posgres database

--- dockerdjango/dockerdjango/settings.py
+++ dockerdjango/dockerdjango/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = "dockerdjango.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "POSTGRES_DB": "MACONDUITE",
+        "POSTGRES_USER": "MICHEL",
+        "POSTGRES_PASSWORD": "MICHELPOSTGRES",
+        # "POSTGRES_HOST": "127.0.0.1",
+        # "POSTGRES_PORT": "5432",
     }
 }
 
